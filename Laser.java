acff1b802d9f5d2c2b27f33b1e5a399bc31ad87d
Oliver-Y <31872294+Oliver-Y@users.noreply.github.com>
2019-11-14 10:21:41 -0500

    new Laser

--- Laser.java
+++ Laser.java
@@ -8,10 +8,14 @@ public class Laser extends ActiveObject {
 	private final double laser_width = 5; 
 	private double speed = -7.5; 
 	DrawingCanvas c; 
+	private Fleet fleet; 
+	private Ship spaceship; 
 		
-	public Laser(double start_x, double start_y, DrawingCanvas canvas) {
+	public Laser(double start_x, double start_y, DrawingCanvas canvas,Ship s, Fleet f) {
 		//laser = new VisibleImage(i, start_x, start_y, laser_width,laser_len,canvas);
 		laser = new FilledRect(start_x,start_y,laser_width,laser_len,canvas); 
+		//spaceship = s; 
+		//fleet = f; 
 		laser.setColor(Color.WHITE);
 		c = canvas; 
 		start(); 
@@ -32,6 +36,12 @@ public class Laser extends ActiveObject {
 	public void run() {
 		while((laser.getX() >= 0 && laser.getX() <= c.getWidth()) && (laser.getY() <= c.getHeight() && laser.getY() >= 0)) {
 			laser.move(0,speed); 
+			
+			//check to see if laser overlaps with Spaceship or Alien
+			//Which means laser has to have the Visible Image of all aliens along with the ship to check. 
+			//fleet.checkLaser(laser); // Overlaps   
+						
+			//Laser will kill both spaceship and alien.
 			
 			pause(50); 
 		}
