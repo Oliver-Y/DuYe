b7cc04bfda4e0d8ba3cf9380e3d745c950182bd5
Oliver-Y <31872294+Oliver-Y@users.noreply.github.com>
2019-11-14 10:05:04 -0500

    2nd

--- Alien.java
+++ Alien.java
@@ -1,5 +1,16 @@
-import objectdraw.ActiveObject;
+import objectdraw.*;
+import java.awt.Image;
 
 public class Alien extends ActiveObject {
+	private static final int INVADER_WIDTH = 40;
+	private static final int INVADER_HEIGHT = 40;
+	VisibleImage alien; 
+	
+	public Alien(Image i, int x, int y, DrawingCanvas canvas) {
+		alien = new VisibleImage(i,x,y,canvas); 
+	}
+	public void die() {
+		alien.removeFromCanvas();
+	}
 
 }
