2315af64306dace9bd6a15765ef6516f8f05bd7f
Alan Du <ydu20@groton.org>
2020-02-24 09:29:57 -0500

    2-24 9:29am

--- TowersOfHanoi/src/TowersOfHanoi.java
+++ TowersOfHanoi/src/TowersOfHanoi.java
@@ -1,7 +1,5 @@
 import objectdraw.*;
-
 import java.awt.*;
-
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -17,16 +15,41 @@ public class TowersOfHanoi extends WindowController implements KeyListener {
 	private int numDisks;
 	private FramedRect base;
 	private ArrayList<Integer[]> moves;
+	private Integer[] tempMove= {0,0};
 	private Location current;
 	private Disk selected;
+	private FramedRect save;
+	private Text saveText;
+	private FramedRect undo;
+	private Text undoText;
+	private FramedRect reset;
+	private Text resetText;
+	private FramedRect autoPlay;
+	private Text APText;
 	
 	//Constants for the window
 	private static final int HEIGHT= 800;
 	private static final int WIDTH = 800;
 	
-	//Initialize Disks 
+	//Initialize Disks
 	public void begin() {
 		base = new FramedRect(50, 650, 700, 50, canvas);
+		
+		save = new FramedRect(520, 30, 70, 25, canvas);
+		saveText = new Text("Save", 539, 33, canvas);
+		saveText.setFontSize(15);
+		
+		undo = new FramedRect(600, 30, 70, 25, canvas);
+		undoText = new Text("Undo", 617, 33, canvas);
+		undoText.setFontSize(15);
+
+		reset = new FramedRect(680, 30, 70, 25, canvas);
+		resetText = new Text("Reset", 695, 33, canvas);
+		resetText.setFontSize(15);
+
+		autoPlay = new FramedRect(520, 65, 230, 25, canvas);
+		APText = new Text("Auto Play", 600, 68, canvas);
+		APText.setFontSize(15);
 		
 		//Initialize Piles
 		lPile = new Pile(90, numDisks, canvas);
@@ -35,11 +58,27 @@ public class TowersOfHanoi extends WindowController implements KeyListener {
 		
 	}
 
+	public void onMouseClick(Location l) {
+		
+	}
+	
 	public void onMousePress(Location l) {
 		current = l;
-		if (lPile.diskContains(l)) selected = lPile.removeTop();
-		else if (mPile.diskContains(l)) selected = mPile.removeTop();
-		else if (rPile.diskContains(l)) selected = rPile.removeTop();
+		if (lPile.diskContains(l)) {
+			selected = lPile.removeTop();
+			tempMove[1] = 1;
+			tempMove[2] = 1;
+		}
+		else if (mPile.diskContains(l)) {
+			selected = mPile.removeTop();
+			tempMove[1] = 2;
+			tempMove[2] = 2;
+		}
+		else if (rPile.diskContains(l)) {
+			selected = rPile.removeTop();
+			tempMove[1] = 3;
+			tempMove[2] = 3;
+		}
 		else selected = null;
 	}
 	
@@ -50,14 +89,25 @@ public class TowersOfHanoi extends WindowController implements KeyListener {
 			selected.move(dx, dy);
 			current = l;
 		}
-		
 	}
 	
 	public void onMouseRelease(Location l) {
 		if (selected != null) {
-			if (lPile.contains(l) && lPile.canAdd(selected)) lPile.addTop(selected);
-			else if (mPile.contains(l)&& mPile.canAdd(selected)) mPile.addTop(selected);
-			else if (rPile.contains(l)&& rPile.canAdd(selected)) rPile.addTop(selected);
+			if (lPile.contains(l) && lPile.canAdd(selected)) {
+				lPile.addTop(selected);
+				tempMove[2] = 1;
+				moves.add(tempMove);
+			}
+			else if (mPile.contains(l)&& mPile.canAdd(selected)) {
+				mPile.addTop(selected);
+				tempMove[2] = 2;
+				moves.add(tempMove);
+			}
+			else if (rPile.contains(l)&& rPile.canAdd(selected)) {
+				rPile.addTop(selected);
+				tempMove[2] = 3;
+				moves.add(tempMove);
+			}
 			else selected.getPile().addTop(selected);;
 		}
 		selected = null;
